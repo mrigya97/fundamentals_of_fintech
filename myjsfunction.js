1f2c18684b065970897adf75e22b44ebf7f78aa7
Mrigya Sharma <34007790+mrigya97@users.noreply.github.com>
2021-05-07 01:02:55 +0100

    Add files via upload

---
+++ myjsfunction.js
@@ -0,0 +1,13 @@
+function changeImage()
+{ 
+   var image = document.getElementBy Id('myImage');
+   if
+     (image.src.match("bitcoinsymbol"))
+      {
+	image.src="C:\Users\Hi\Desktop\bitcoin_image.png";
+      }
+     else
+     {
+	image.src="C:\Users\Hi\Desktop\bitcoin_image.png";
+     }
+}
